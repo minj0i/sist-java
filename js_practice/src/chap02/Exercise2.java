97f9773038553ed02a496566d0e05f0b323f23d7
minj0i <h_hoh@naver.com>
2019-01-10 18:06:40 +0900

    0110

---
+++ js_practice/src/chap02/Exercise2.java
@@ -0,0 +1,50 @@
+package chap02;
+
+public class Exercise2 {
+	public void Excercise2() {
+		
+	}
+//2-1
+//	boolean
+//			char		String
+//	byte	short		int			long
+//						float		double	
+
+//2-2
+//	long regNo =7701011111222L;
+	
+//2-3
+//	int i = 100;
+//	long l = 100L;
+//	final float PI = 3.14f;
+	
+//	리터럴:100, 100L, 3.14f
+//	변수 : i, l
+//	키워드: int, long, final, float
+//	상수 : PI
+	
+//2-4
+// b
+//	기본형은 boolean, byte, short, char, int, long, float, double
+
+//2-5
+	
+//12
+//true
+//131
+//51
+//99
+//Java
+//오류
+	
+	public static void main(String[] args) {
+		System.out.println("1" + "2");
+		System.out.println(true + "");
+		System.out.println('A' + 'B');
+		System.out.println('1' + 2);
+		System.out.println('1' + '2');
+		System.out.println('J' + "ava");
+//		System.out.println(true + null);
+	}
+	
+}//class
