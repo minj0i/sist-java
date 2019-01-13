2555581c4d163f9db9b810a46fecbc66cf369e4f
minj0i <h_hoh@naver.com>
2019-01-13 21:34:13 +0900

    0113

---
+++ js_practice/src/logInDb/loginView.java
@@ -0,0 +1,62 @@
+package logInDb;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class loginView extends JFrame{
+	private JTextField jfId, jfPass;
+	private JButton jbtLogin;
+	
+	public loginView() {
+		super("로그인");
+		jfId = new JTextField(10); 
+		jfPass = new JTextField(10);
+		jbtLogin = new JButton("로그인");
+		
+		JPanel jpl = new JPanel();
+		jpl.setLayout(new GridLayout(2,2));
+		
+		jpl.add(new JLabel("아이디"));
+		jpl.add(jfId);
+		jpl.add(new JLabel("비밀번호"));
+		jpl.add(jfPass);
+		
+		add("Center", jpl);
+		add("South", jbtLogin);
+		loginEvt le = new loginEvt(this);
+		
+		jbtLogin.addActionListener(le);
+		jfId.addActionListener(le);
+		jfPass.addActionListener(le);
+		addWindowListener(le);
+		
+		setBounds(100, 100, 200, 200);
+		setVisible(true);
+	}
+	
+	
+	public JTextField getJfId() {
+		return jfId;
+	}
+
+
+	public JTextField getJfPass() {
+		return jfPass;
+	}
+
+
+	public JButton getJbtLogin() {
+		return jbtLogin;
+	}
+
+
+	public static void main(String[] args) {
+		new loginView();
+	}
+}
