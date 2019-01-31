96aa8a4cfc2e994452aeaa970a9a2b2d3047af7b
minj0i <h_hoh@naver.com>
2019-01-31 17:03:19 +0900

    0131

---
+++ html_prj/WebContent/js0131/external.js
@@ -0,0 +1,7 @@
+//.js파일을 만들고 그 안에서 javaScript code를 정의하는 방식
+//필요한 HTML에서 <script type="text/javascript" src=".js파일 URL"></script>
+//태그를 정의하여 사용한다.
+
+function ext(){
+	alert("external File방식");
+}//ext
